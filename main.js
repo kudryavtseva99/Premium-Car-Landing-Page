9b629a0b18325a9d39eaa2d71e5900398154959c
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2023-12-13 20:37:01 +0600

    Make adaptive layout for mobile (except article section "feed")

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-const mobileButtonMenu = document.querySelector(".header_burger");
-mobileButtonMenu.addEventListener("click", () => {
-  mobileButtonMenu.classList.toggle("active");
-});
+(function () {
+  const mobileButtonMenu = document.querySelector(".header_burger");
+  const menuLinks = document.querySelectorAll(".header_link");
+  mobileButtonMenu.addEventListener("click", () => {
+    mobileButtonMenu.classList.add("active span");
+  });
+  mobileButtonMenu.addEventListener("click", () => {
+    mobileButtonMenu.classList.remove(".active span");
+  });
+
+  if (window.innerHeight <= 1024) {
+    for (let i = 0; i < menuLinks.length; i += 1) {
+      menuLinks[i].addEventListener("click", () => {
+        mobileButtonMenu.classList.remove(".active span");
+      });
+    }
+  }
+})();
