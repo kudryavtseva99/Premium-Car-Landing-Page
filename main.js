a7dca87989c840cc9c31ec4c040eaa227e4f566c
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2023-12-14 22:26:44 +0600

    Activate the burger menu button: - add eventlistener -add function disableScroll (doesn't work) - add styles dis-scroll, overlay, header_nav-visible

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-(function () {
-  const mobileButtonMenu = document.querySelector(".header_burger");
-  const menuLinks = document.querySelectorAll(".header_link");
-  mobileButtonMenu.addEventListener("click", () => {
-    mobileButtonMenu.classList.add("active span");
-  });
-  mobileButtonMenu.addEventListener("click", () => {
-    mobileButtonMenu.classList.remove(".active span");
-  });
+const mobileButton = document.querySelector(".header_burger");
+const navigation = document.querySelector(".header_navigation-burger");
+const overlay = document.querySelector(".overlay");
+const body = document.body;
 
-  if (window.innerHeight <= 1024) {
-    for (let i = 0; i < menuLinks.length; i += 1) {
-      menuLinks[i].addEventListener("click", () => {
-        mobileButtonMenu.classList.remove(".active span");
-      });
-    }
-  }
-})();
+function disableScroll() {
+  body.classList.contains("dis-scroll")
+    ? body.classList.remove("dis-scroll")
+    : body.classList.add("dis-scroll");
+}
+mobileButton.addEventListener("click", () => {
+  mobileButton.classList.toggle("active");
+  navigation.classList.toggle("header_nav-visible");
+  overlay.classList.toggle("overlay-visible");
+});
