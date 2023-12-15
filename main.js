7fe6d73b0a073df60dba33aacb83b038d2e561f5
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2023-12-15 20:59:02 +0600

    Add function toggleScroll and Event Listener - add margin top for header_list

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ const navigation = document.querySelector(".header_navigation-burger");
 const overlay = document.querySelector(".overlay");
 const body = document.body;
 
-function disableScroll() {
+function toggleScroll() {
   body.classList.contains("dis-scroll")
     ? body.classList.remove("dis-scroll")
     : body.classList.add("dis-scroll");
 }
+
 mobileButton.addEventListener("click", () => {
   mobileButton.classList.toggle("active");
   navigation.classList.toggle("header_nav-visible");
   overlay.classList.toggle("overlay-visible");
 });
+mobileButton.addEventListener("click", toggleScroll);
